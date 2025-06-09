2ec0587220c3cde4e44cc712d6c3a1311a0a910d
vincent@lougardian.ovh <vincent@lougardian.ovh>
2025-06-09 21:53:50 +0000

    ajout commentaire premier fichier de moèle

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -7,6 +7,20 @@
     Try changing "table" to "view" below
 */
 
+
+
+/*
+{{ config(materialized='table') }} : Il s'agit d'une configuration dbt qui indique à dbt de matérialiser les résultats de la requête sous forme de table dans la base de données. D'autres options de matérialisation incluent view, incremental, et ephemeral.
+
+
+La clause with est utilisée pour définir une Expression de Table Commune (CTE). Dans ce cas, elle définit une CTE nommée source_data.
+
+
+source_data : Il s'agit de la CTE qui contient les données définies par la requête suivante :
+
+*/
+
+
 {{ config(materialized='table') }}
 
 with source_data as (
